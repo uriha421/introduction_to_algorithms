d29c688453e343a80306b0bca8abd07c5b85f06d
uriha421 <uriha421@gmail.com>
2018-08-07 21:58:53 +0900

    add comments in insertion_sort.c

--- I_Foundations/insertion_sort.c
+++ I_Foundations/insertion_sort.c
@@ -1,16 +1,18 @@
 // C program for an insertion-sort algorithm.
+// input: an integer array such as 1 3 4 2 9 7.
+// output: a sorted array, 1 2 3 4 7 9.
 #include "stdio.h"
 #include "stdlib.h"
 
 int main ( int argc, char *argv[] ) {
 
-  // a receives command-line arguments
+  // a receives command-line arguments.
   int a[argc - 1];
   for (int n = 0; n < argc - 1; n++) {
     a[n] = atoi(argv[n + 1]);
   }
 
-  // insertion-sort algorithm
+  // insertion-sort algorithm.
   int k, m;
   for (int n = 1; n < argc - 1; n++) {
     k = a[n];
