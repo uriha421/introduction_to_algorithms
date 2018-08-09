536edf7389f7fa2279433078ea3e96d63feab67e
uriha421 <uriha421@gmail.com>
2018-08-09 23:38:39 +0900

    add and fix comments in selection_sort.c

--- I_Foundations/selection_sort.c
+++ I_Foundations/selection_sort.c
@@ -1,4 +1,4 @@
-// C program for an insertion-sort algorithm.
+// C program for a selection-sort algorithm.
 // input: an integer array such as 1 3 4 2 9 7.
 // output: a sorted array, 1 2 3 4 7 9.
 #include "stdio.h"
@@ -12,17 +12,18 @@ int main ( int argc, char *argv[] ) {
     a[n] = atoi(argv[n + 1]);
   }
 
-  // insertion-sort algorithm.
+  // selection-sort algorithm.
   int k, l;
   for (int n = 0; n < argc - 2; n++) {
-    k = a[n];
-    l = n;
+    k = a[n]; // k will be the least number among a[n ...]
+    l = n; // l will be the index of the least number
     for (int m = n + 1; m < argc - 1; m++) {
       if (a[n] > a[m]) {
         k = a[m];
         l = m;
       }
     }
+    // switches a[n] and a[l], then a[1 ... n] has been sorted.
     a[l] = a[n];
     a[n] = k;
   }
